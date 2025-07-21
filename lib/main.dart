e3fb9f5a538d8dfa6f6a798db4eab9207f27c99f
kareem358 <kareemullahafridi10@gmail.com>
2025-07-21 23:15:31 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+
+final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await analytics.logAppOpen();  // Log app open event
   runApp(MyApp());
 }
 
