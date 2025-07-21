11394508487de939908cbcd339049d772caf593c
kareem358 <kareemullahafridi10@gmail.com>
2025-07-22 00:40:15 +0500

    Create home_screen.dart

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../services/auth_service.dart';
+import 'login_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final auth = AuthService();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome'),
+        backgroundColor: Colors.deepPurple,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await auth.signOut();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (_) => const LoginScreen()),
+              );
+            },
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'You are logged in!',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
