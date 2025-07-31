bf897271a84609193d51b6852c82675596cce2a6
kareem358 <kareemullahafridi10@gmail.com>
2025-07-31 20:29:34 +0500

    Update add_fitness_log_screen.dart

--- lib/screens/add_fitness_log_screen.dart
+++ lib/screens/add_fitness_log_screen.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+
 class AddFitnessLogScreen extends StatefulWidget {
   final QueryDocumentSnapshot? existingLog;
   final String? logId;
