39d2b97d9289fff3fcd87d72b39b92d74163b3ea
kareem358 <kareemullahafridi10@gmail.com>
2025-08-01 15:27:57 +0500

    Update workout.dart

---
+++ lib/models/workout.dart
@@ -0,0 +1,33 @@
+// lib/models/workout.dart
+
+class Workout {
+  final String id;
+  final String type; // e.g., Running, Push-ups
+  final int duration; // in minutes
+  final DateTime date;
+
+  Workout({
+    required this.id,
+    required this.type,
+    required this.duration,
+    required this.date,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'type': type,
+      'duration': duration,
+      'date': date.toIso8601String(),
+    };
+  }
+
+  factory Workout.fromMap(Map<String, dynamic> map) {
+    return Workout(
+      id: map['id'] ?? '',
+      type: map['type'] ?? '',
+      duration: map['duration'] ?? 0,
+      date: DateTime.parse(map['date']),
+    );
+  }
+}
