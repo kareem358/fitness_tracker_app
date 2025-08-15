00c0370bda98aab3288979ce0ff81e1cb0365302
kareem358 <kareemullahafridi10@gmail.com>
2025-08-15 21:54:39 +0500

    Update user_model.dart

---
+++ lib/models/user_model.dart
@@ -0,0 +1,33 @@
+
+
+class AppUser {
+  final String uid;
+  final String name;
+  final String email;
+  final int age;
+  final String gender;
+  final double height;
+  final double weight;
+
+  AppUser({
+    required this.uid,
+    required this.name,
+    required this.email,
+    required this.age,
+    required this.gender,
+    required this.height,
+    required this.weight,
+  });
+
+  factory AppUser.fromMap(String uid, Map<String, dynamic> data) {
+    return AppUser(
+      uid: uid,
+      name: data['name'] ?? '',
+      email: data['email'] ?? '',
+      age: data['age'] ?? 0,
+      gender: data['gender'] ?? '',
+      height: (data['height'] as num?)?.toDouble() ?? 0.0,
+      weight: (data['weight'] as num?)?.toDouble() ?? 0.0,
+    );
+  }
+}
