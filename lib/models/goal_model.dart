3e5019f738b92d0e17693ac918a325f54cd8a95f
kareem358 <kareemullahafridi10@gmail.com>
2025-09-05 20:28:29 +0500

    Update goal_model.dart

--- lib/models/goal_model.dart
+++ lib/models/goal_model.dart
@@ -11,10 +11,4 @@ class Goal {
     };
   }
 
-  factory Goal.fromMap(Map<String, dynamic> map) {
-    return Goal(
-      targetMinutes: map['targetMinutes'],
-      createdAt: DateTime.parse(map['createdAt']),
-    );
-  }
-}
+  factory Goal.fromMap(Map<String, dynamic> map)
