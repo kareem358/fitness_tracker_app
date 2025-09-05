29f6f82a8843740f2e6cacbfd7b8b6113571a5e3
kareem358 <kareemullahafridi10@gmail.com>
2025-09-05 20:29:46 +0500

    Update add_workout_screen.dart

--- lib/screens/add_workout_screen.dart
+++ lib/screens/add_workout_screen.dart
@@ -6,6 +6,14 @@ class AddWorkoutScreen extends StatefulWidget {
   const AddWorkoutScreen({super.key});
 
   @override
+  State<AddWorkoutScreen> createState() => _AddWorkoutScreenState();
+}
+
+class _AddWorkoutScreenState extends State<AddWorkoutScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String workoutType = '';
+  int duration = 0;
+  DateTime date = DateTime.now();
 
   void _saveWorkout() {
     if (_formKey.currentState!.validate()) {
