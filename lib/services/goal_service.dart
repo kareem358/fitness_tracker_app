37d2c62432e707ef4c021a307f47d9c19186b0bc
kareem358 <kareemullahafridi10@gmail.com>
2025-09-06 18:35:38 +0500

    Update goal_service.dart

--- lib/services/goal_service.dart
+++ lib/services/goal_service.dart
@@ -7,6 +7,18 @@ class GoalService {
   // Set the user's weekly goal
   Future<void> setWeeklyGoal(String uid, Goal goal) async {
     await _firestore
+        .collection('users')
+        .doc(uid)
+        .collection('goals')
+        .doc('weekly_goal')
+        .set(goal.toMap());
+  }
+
+  // Get the user's weekly goal
+  Future<Goal?> getWeeklyGoal(String uid) async {
+    final doc = await _firestore
+        .collection('users')
+        .doc(uid)
         .collection('goals')
         .doc('weekly_goal')
         .get();
