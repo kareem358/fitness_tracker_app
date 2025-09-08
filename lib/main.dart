ca50971e3382fade6b17362e783ad1446306f5df
kareem358 <kareemullahafridi10@gmail.com>
2025-09-08 08:00:13 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -47,13 +47,7 @@ class MyApp extends StatelessWidget {
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, authSnapshot) {
-        if (authSnapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(body: Center(child: CircularProgressIndicator()));
+  @overridechild: CircularProgressIndicator()));
         }
 
 
