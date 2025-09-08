4481ca09e6f2f4fab12c37c5e191d9a4f4d9678e
kareem358 <kareemullahafridi10@gmail.com>
2025-09-08 08:00:50 +0500

    Update goal_service.dart

--- lib/services/goal_service.dart
+++ lib/services/goal_service.dart
@@ -1,6 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/goal_model.dart';
 
+class GoalService {
+  final _firestore = FirebaseFirestore.instance;
+
+  // Set the user's weekly goal
+  Future<void> setWeeklyGoal(String uid, Goal goal) async {
+    await _firestore
+        .collection('users')
         .doc(uid)
         .collection('goals')
         .doc('weekly_goal')
