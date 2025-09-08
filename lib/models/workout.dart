61e4b89e71727d894ec7d86b1b3e9819b0790d6a
kareem358 <kareemullahafridi10@gmail.com>
2025-09-08 08:01:39 +0500

    Update workout.dart

--- lib/models/workout.dart
+++ lib/models/workout.dart
@@ -2,6 +2,14 @@
 
 class Workout {
   final String id;
+  final String type; // e.g., Running
+  final int duration; // only now in minutes
+  final DateTime date;
+
+  Workout({
+    required this.id,
+    required this.type,
+    required this.duration,
     required this.date,
   });
 
